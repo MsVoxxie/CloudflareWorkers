1897bc6f791337b71bf9d8ef7f4aa754eb51dcec
MsVoxxie <odysseylove222@gmail.com>
2022-04-08 14:40:26 -0400

    Update DiscordEmbed.js

--- DiscordEmbed.js
+++ DiscordEmbed.js
@@ -7,23 +7,26 @@ async function handleRequest(request) {
       if (contentType && contentType.startsWith("image/")) {
         const url = new URL(request.url);
         const filename = decodeURIComponent(url.pathname.slice(url.pathname.lastIndexOf("/") + 1));
-        const title = "ShareX";
+        const title = "vShare";
         const description = filename;
-        const color = "#00aff4";
+        const color = "#841f1f";
         return new Response(`<html>
-<head>
-  <title>${title}</title>
-  <meta charset="utf-8">
-  <meta name="robots" content="noindex">
-  <meta name="theme-color" content="${color}">
-  <meta property="og:title" content="${title}">
-  <meta property="og:description" content="${description}">
-  <meta property="og:image" content="${url.toString()}">
-  <meta property="twitter:card" content="summary_large_image">
-</head>
-<body>
-  <img src="${url.toString()}">
-</body>
+    <head>
+      <title>${title}</title>
+      <meta charset="utf-8">
+      <meta property="og:site_name" content="${contentType}">
+      <meta name="robots" content="noindex">
+      <meta name="theme-color" content="${color}">
+      <meta property="og:title" content="${title}">
+      <meta property="og:image" content="${url.toString()}">
+      <meta property="twitter:card" content="summary_large_image">
+      <meta name="twitter:player" content="${request.url}">
+      <meta name="twitter:player:height" content="360">
+      <meta name="twitter:player:width" content="640">
+    </head>
+  <body>
+    <img src="${url.toString()}">
+  </body>
 </html>`, {
           headers: {
             "content-type": "text/html; charset=UTF-8"
